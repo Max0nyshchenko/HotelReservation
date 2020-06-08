317eadaffde8e957ea76635c8d52a3eb4c5d80a2
Max0nyshchenko <max.0nyshenk03@gmail.com>
2020-06-08 18:20:58 +0300

    comit

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "../components/Room";
+import Title from "./Title";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    return <div>Hello from FeatjRooms </div>;
+    const { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
+    });
+    return (
+      <section className="featured-rooms">
+        <Loading />
+      </section>
+    );
   }
 }
--- src/context.js
+++ src/context.js
@@ -13,12 +13,19 @@ export default class RoomProvider extends Component {
 
   componentDidMount() {
     let rooms = this.formatData(items);
+    let featuredRooms = rooms.filter((room) => room.featured === true);
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms: rooms,
+      loading: false,
+    });
   }
 
   formatData(items) {
     let tempItems = items.map((item) => {
       let id = item.sys.id;
-      let images = item.fileds.images.map((image) => image.fields.file.url);
+      let images = item.fields.images.map((image) => image.fields.file.url);
       let room = { ...item.fields, images, id };
 
       return room;
