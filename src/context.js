b914c37d75d3c6cd35dc05591c5efd48620b9c71
Max0nyshchenko <max.0nyshenk03@gmail.com>
2020-06-26 17:07:45 +0300

    final commit V2

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import items from "./data";
+import Client from "./Contentful";
 
 const RoomContext = React.createContext();
 
@@ -22,20 +23,32 @@ export default class RoomProvider extends Component {
     reservedRoomsCount: 0,
   };
 
+  // get data from contentful
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: "hotelReservation",
+      });
+      let rooms = this.formatData(response.items);
+      let featuredRooms = rooms.filter((room) => room.featured === true);
+      let maxPrice = Math.max(...rooms.map((item) => item.price));
+      let maxSize = Math.max(...rooms.map((item) => item.size));
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        price: maxPrice,
+        maxPrice,
+        maxSize,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   componentDidMount() {
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter((room) => room.featured === true);
-    let maxPrice = Math.max(...rooms.map((item) => item.price));
-    let maxSize = Math.max(...rooms.map((item) => item.size));
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      price: maxPrice,
-      maxPrice,
-      maxSize,
-    });
+    this.getData();
   }
 
   formatData(items) {
@@ -55,23 +68,6 @@ export default class RoomProvider extends Component {
     });
     return room;
   };
-
-  // rent = (event) => {
-  //   let id;
-  //   console.log(event.target.parentElement.parentElement);
-  //   if (event.target.className === "rent-btn") {
-  //     id = event.target.id;
-  //   } else if (
-  //     event.target.className === "room-txt" ||
-  //     event.target.className === "room-icon"
-  //   ) {
-  //     id = event.target.parentElement.id;
-  //   }
-  //   this.setState((prev) => {
-  //     return { reservedRooms: [...prev.reservedRooms, id] };
-  //   });
-  //   console.log(this.state.reservedRooms);
-  // };
 
   rent = () => {
     this.setState((prev) => {
